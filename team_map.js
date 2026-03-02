3777ed8ffd6e67285515353ee4b2fcc07525c414
znye1 <zhiyanye1@gmail.com>
2026-03-01 22:47:46 -0500

    Add TEAM_MAP for various sports teams

---
+++ team_map.js
@@ -0,0 +1,322 @@
+export const TEAM_MAP = {
+    "ALA": {
+        "Alabama": "Alabama"
+    },
+    "ANA": {
+        "Ducks": "Anaheim Ducks"
+    },
+    "ARI": {
+        "Cardinals": "Arizona Cardinals"
+    },
+    "ARIZ": {
+        "Arizona": "Arizona"
+    },
+    "ARK": {
+        "Arkansas": "Arkansas"
+    },
+    "ATL": {
+        "Falcons": "Atlanta Falcons",
+        "Hawks": "Atlanta Hawks"
+    },
+    "BAL": {
+        "Ravens": "Baltimore Ravens"
+    },
+    "BKN": {
+        "Nets": "Brooklyn Nets"
+    },
+    "BOS": {
+        "Bruins": "Boston Bruins",
+        "Celtics": "Boston Celtics"
+    },
+    "BUF": {
+        "Bills": "Buffalo Bills",
+        "Sabres": "Buffalo Sabres"
+    },
+    "BYU": {
+        "BYU": "BYU"
+    },
+    "CAR": {
+        "Hurricanes": "Carolina Hurricanes",
+        "Panthers": "Carolina Panthers"
+    },
+    "CBJ": {
+        "Blue Jackets": "Columbus Blue Jackets"
+    },
+    "CGY": {
+        "Flames": "Calgary Flames"
+    },
+    "CHA": {
+        "Hornets": "Charlotte Hornets"
+    },
+    "CHI": {
+        "Bears": "Chicago Bears",
+        "Blackhawks": "Chicago Blackhawks",
+        "Bulls": "Chicago Bulls"
+    },
+    "CIN": {
+        "Bengals": "Cincinnati Bengals"
+    },
+    "CLE": {
+        "Browns": "Cleveland Browns",
+        "Cavaliers": "Cleveland Cavaliers"
+    },
+    "COL": {
+        "Avalanche": "Colorado Avalanche"
+    },
+    "CONN": {
+        "UConn": "Connecticut"
+    },
+    "DAL": {
+        "Cowboys": "Dallas Cowboys",
+        "Mavs": "Dallas Mavericks",
+        "Stars": "Dallas Stars"
+    },
+    "DEN": {
+        "Broncos": "Denver Broncos",
+        "Nuggets": "Denver Nuggets"
+    },
+    "DET": {
+        "Lions": "Detroit Lions",
+        "Pistons": "Detroit Pistons",
+        "Red Wings": "Detroit Red Wings"
+    },
+    "DUKE": {
+        "Duke": "Duke"
+    },
+    "EDM": {
+        "Oilers": "Edmonton Oilers"
+    },
+    "FLA": {
+        "Florida": "Florida",
+        "Panthers": "Florida Panthers"
+    },
+    "GB": {
+        "Packers": "Green Bay Packers"
+    },
+    "GSW": {
+        "Warriors": "Golden State Warriors"
+    },
+    "HOU": {
+        "Houston": "Houston",
+        "Rockets": "Houston Rockets",
+        "Texans": "Houston Texans"
+    },
+    "ILL": {
+        "Illinois": "Illinois"
+    },
+    "IND": {
+        "Colts": "Indianapolis Colts",
+        "Indiana": "Indiana",
+        "Pacers": "Indiana Pacers"
+    },
+    "IOWA": {
+        "Iowa": "Iowa"
+    },
+    "ISU": {
+        "ISU": "Iowa State"
+    },
+    "JAX": {
+        "Jaguars": "Jacksonville Jaguars"
+    },
+    "KC": {
+        "Chiefs": "Kansas City Chiefs"
+    },
+    "KU": {
+        "Kansas": "Kansas"
+    },
+    "LAC": {
+        "Chargers": "Los Angeles Chargers",
+        "Clippers": "Los Angeles Clippers"
+    },
+    "LAK": {
+        "Kings": "Los Angeles Kings"
+    },
+    "LAL": {
+        "Lakers": "Los Angeles Lakers"
+    },
+    "LAR": {
+        "Rams": "Los Angeles Rams"
+    },
+    "LV": {
+        "Raiders": "Las Vegas Raiders"
+    },
+    "MEM": {
+        "Grizzlies": "Memphis Grizzlies"
+    },
+    "MIA": {
+        "Dolphins": "Miami Dolphins",
+        "Heat": "Miami Heat",
+        "Miami": "Miami Florida"
+    },
+    "MICH": {
+        "Michigan": "Michigan"
+    },
+    "MIL": {
+        "Bucks": "Milwaukee Bucks"
+    },
+    "MIN": {
+        "Vikings": "Minnesota Vikings",
+        "Wild": "Minnesota Wild",
+        "Wolves": "Minnesota Timberwolves"
+    },
+    "MISS": {
+        "Ole Miss": "Ole Miss"
+    },
+    "MSU": {
+        "MSU": "Michigan State"
+    },
+    "MTL": {
+        "Canadiens": "Montreal Canadiens"
+    },
+    "NE": {
+        "Patriots": "New England Patriots"
+    },
+    "NEB": {
+        "Nebraska": "Nebraska"
+    },
+    "NJD": {
+        "Devils": "New Jersey Devils"
+    },
+    "NO": {
+        "Saints": "New Orleans Saints"
+    },
+    "NOP": {
+        "Pelicans": "New Orleans Pelicans"
+    },
+    "NSH": {
+        "Predators": "Nashville Predators"
+    },
+    "NYG": {
+        "Giants": "New York Giants"
+    },
+    "NYI": {
+        "Islanders": "New York Islanders"
+    },
+    "NYJ": {
+        "Jets": "New York Jets"
+    },
+    "NYK": {
+        "Knicks": "New York Knicks"
+    },
+    "NYR": {
+        "Rangers": "New York Rangers"
+    },
+    "OKC": {
+        "Thunder": "Oklahoma City Thunder"
+    },
+    "ORE": {
+        "Oregon": "Oregon"
+    },
+    "ORL": {
+        "Magic": "Orlando Magic"
+    },
+    "OSU": {
+        "OSU": "Ohio State"
+    },
+    "OTT": {
+        "Senators": "Ottawa Senators"
+    },
+    "OU": {
+        "Oklahoma": "Oklahoma"
+    },
+    "PHI": {
+        "76ers": "Philadelphia 76ers",
+        "Eagles": "Philadelphia Eagles",
+        "Flyers": "Philadelphia Flyers"
+    },
+    "PIT": {
+        "Penguins": "Pittsburgh Penguins",
+        "Steelers": "Pittsburgh Steelers"
+    },
+    "PHX": {
+        "Suns": "Phoenix Suns"
+    },
+    "POR": {
+        "Blazers": "Portland Trail Blazers"
+    },
+    "SAC": {
+        "Kings": "Sacramento Kings"
+    },
+    "SAS": {
+        "Spurs": "San Antonio Spurs"
+    },
+    "SEA": {
+        "Kraken": "Seattle Kraken",
+        "Seahawks": "Seattle Seahawks"
+    },
+    "SF": {
+        "49ers": "San Francisco 49ers"
+    },
+    "SJS": {
+        "Sharks": "San Jose Sharks"
+    },
+    "SJU": {
+        "SJU": "St. John's"
+    },
+    "STA": {
+        "Stars": "USA Stars"
+    },
+    "STL": {
+        "Blues": "St. Louis Blues"
+    },
+    "STR": {
+        "Stripes": "USA Stripes"
+    },
+    "TA&M": {
+        "Texas A&M": "Texas A&M"
+    },
+    "TB": {
+        "Buccaneers": "Tampa Bay Buccaneers"
+    },
+    "TBL": {
+        "Lightning": "Tampa Bay Lightning"
+    },
+    "TEN": {
+        "Titans": "Tennessee Titans"
+    },
+    "TENN": {
+        "Tennessee": "Tennessee"
+    },
+    "TOR": {
+        "Maple Leafs": "Toronto Maple Leafs",
+        "Raptors": "Toronto Raptors"
+    },
+    "TTU": {
+        "TTU": "Texas Tech"
+    },
+    "UK": {
+        "Kentucky": "Kentucky"
+    },
+    "UNC": {
+        "UNC": "North Carolina"
+    },
+    "UGA": {
+        "Georgia": "Georgia"
+    },
+    "UTA": {
+        "Jazz": "Utah Jazz",
+        "Mammoth": "Utah Mammoth"
+    },
+    "VAN": {
+        "Canucks": "Vancouver Canucks"
+    },
+    "VGK": {
+        "Golden Knights": "Vegas Golden Knights"
+    },
+    "WAS": {
+        "Commanders": "Washington Commanders",
+        "Wizards": "Washington Wizards"
+    },
+    "WISC": {
+        "Wisconsin": "Wisconsin"
+    },
+    "WLD": {
+        "World": "World"
+    },
+    "WPG": {
+        "Jets": "Winnipeg Jets"
+    },
+    "WSH": {
+        "Capitals": "Washington Capitals"
+    }
+};
